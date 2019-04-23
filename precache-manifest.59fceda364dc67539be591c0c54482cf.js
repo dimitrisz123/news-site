bbaf1a58714bf979c517f8eab5bd085a6798c725
Δημήτρης Ζέρβας <zervasd@MINISTRY.DOM>
2019-04-23 11:49:38 +0300

    Updates

---
+++ precache-manifest.59fceda364dc67539be591c0c54482cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cce5f16d52406b75ce7",
+    "url": "/news-site/static/js/runtime~main.70ac7bbb.js"
+  },
+  {
+    "revision": "72ed2e3e165fde739ee1",
+    "url": "/news-site/static/js/main.f770029f.chunk.js"
+  },
+  {
+    "revision": "7e8ee6656ef1743846ed",
+    "url": "/news-site/static/js/2.251b0d6d.chunk.js"
+  },
+  {
+    "revision": "ce11c21ec48b0c3b49f61053222193b6",
+    "url": "/news-site/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/news-site/precache-manifest.854726e3c0f5ed6e2503aec4b74d64bf.js"
+  "/news-site/precache-manifest.59fceda364dc67539be591c0c54482cf.js"
 );
 
 workbox.clientsClaim();
